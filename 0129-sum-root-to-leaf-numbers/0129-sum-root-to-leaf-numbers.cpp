99925f804b9b1d4d33b054a2b2c7771c257a51fe
mdalamrahman4 <98524939+mdalamrahman4@users.noreply.github.com>
2024-01-08 13:47:56 +0530

    Time: 4 ms (32.04%), Space: 9.7 MB (25.98%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -11,17 +11,35 @@
  */
 class Solution {
 public:
-    int check(TreeNode*root,int curr)
-    {
-        if(!root) return 0;
-        curr=curr*10+root->val;
-        if(root->left==NULL && root->right==NULL)
-        {
-            return curr;
-        }
-        return check(root->left,curr)+check(root->right,curr);
-    }
     int sumNumbers(TreeNode* root) {
-        return check(root,0);
+        TreeNode*curr=root;
+        int totsum=0,currsum=0,depth=0;
+        while(curr)
+        {
+            if(curr->left==NULL)
+            {
+                currsum=currsum*10+curr->val;
+                if(curr->right==NULL) {totsum+=currsum;}
+                curr=curr->right;
+            }
+            else{
+                TreeNode*prev=curr->left;
+                depth=1;
+                while(prev->right && prev->right!=curr){ prev=prev->right,depth++;}
+                if(prev->right==NULL)
+                {
+                    prev->right=curr;
+                    currsum=currsum*10+curr->val;
+                    curr=curr->left;
+                }
+                else{
+                    prev->right=NULL;
+                    if(prev->left==NULL ) totsum+=currsum;
+                    currsum=currsum/pow(10,depth);
+                    curr=curr->right;
+                }
+            }
+        }
+            return totsum;
     }
 };
