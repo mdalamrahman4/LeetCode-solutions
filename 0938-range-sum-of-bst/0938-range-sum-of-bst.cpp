36922039abd4c3010a789d50015b3bd04e8b1c40
mdalamrahman4 <98524939+mdalamrahman4@users.noreply.github.com>
2024-01-08 11:30:39 +0530

    Time: 103 ms (39.43%), Space: 65 MB (51.38%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -1,44 +1,36 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
-        int sum=0;
-        TreeNode*curr=root;
-        while(curr)
-        {
-            if(curr->left==NULL)
-            {
-                if(curr->val>=low && curr->val<=high) sum+=curr->val;
-                curr=curr->right;
-            }
-            else{
-                TreeNode*prev=curr->left;
-                while(prev->right && prev->right!=curr)
-                {
-                    prev=prev->right;
-                }
-                if(prev->right==NULL)
-                {
-                    prev->right=curr;
-                    curr=curr->left;
-                }
-                else{
-                    prev->right=NULL;
-                    if(curr->val>=low && curr->val<=high) sum+=curr->val;
-                    curr=curr->right;
+        int sum = 0;
+        TreeNode* curr = root;
+
+        while (curr) {
+            if (curr->val < low) {
+                curr = curr->right;  
+            } else if (curr->val > high) {
+                curr = curr->left;   
+            } else {
+                if (curr->left == nullptr) {
+                    sum += curr->val;
+                    curr = curr->right;
+                } else {
+                    TreeNode* prev = curr->left;
+                    while (prev->right && prev->right != curr) {
+                        prev = prev->right;
+                    }
+
+                    if (prev->right == nullptr) {
+                        prev->right = curr;
+                        curr = curr->left;
+                    } else {
+                        prev->right = nullptr;
+                        sum += curr->val;
+                        curr = curr->right;
+                    }
                 }
             }
         }
+
         return sum;
     }
 };
