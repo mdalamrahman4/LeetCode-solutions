07da67e53cc2752d5dc3e50270d48dd0d0af8ffa
mdalamrahman4 <98524939+mdalamrahman4@users.noreply.github.com>
2024-01-12 11:46:40 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (72.81%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    void check(int ind,vector<vector<int>>&ans,vector<int>nums) {
-        if(ind==nums.size()){
+    void check(int ind,vector<vector<int>>&ans,vector<int>nums,int sq) {
+        if(ind==sq-1){
             ans.push_back(nums);
             return;
         }
-        for(int i=ind;i<nums.size();i++) {
+        for(int i=ind;i<sq;i++) {
             if(i!=ind && nums[ind]==nums[i]) continue;
             swap(nums[ind],nums[i]);
-            check(ind+1,ans,nums);
+            check(ind+1,ans,nums,sq);
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
-        check(0,ans,nums);
+        check(0,ans,nums,nums.size());
         return ans;
     }
 };
