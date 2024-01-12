bb5651099ecf76a26f54582b128b093abd0b9032
mdalamrahman4 <98524939+mdalamrahman4@users.noreply.github.com>
2024-01-12 09:41:55 +0530

    Time: 0 ms (100.00%), Space: 7 MB (72.68%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    int isvowel(char ch)
-    {
-        string str="aeiouAEIOU";
-        return (str.find(ch)!=string::npos);
-    }
     bool halvesAreAlike(string s) {
-        int c1=0,c2=0;
-        int n=s.length()/2;
+        int c=0;
+        int n=s.size();
         for(int i=0;i<n;i++)
         {
-            if(isvowel(s[i])) c1++;
+            s[i]=tolower(s[i]);
+            if(i<n/2)
+            {
+                if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
+                {
+                    c++;
+                }
+            }
+            else
+            {
+                if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
+                {
+                    c--;
+                }
+            }
         }
-        for(int i=n;i<s.length();i++)
-        {
-            if(isvowel(s[i])) c2++;
-        }
-        return (c1==c2);
+        return (c==0);
     }
 };
