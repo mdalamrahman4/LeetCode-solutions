f578b50521aa4a1967f0039aecbcaccadce97bce
mdalamrahman4 <98524939+mdalamrahman4@users.noreply.github.com>
2024-01-25 21:20:50 +0530

    Time: 55 ms (52.54%), Space: 11.2 MB (81.42%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int n=p.size(),m=s.size();
+        int m=s.size(),n=p.size();
         vector<bool>prev(m+1,false),curr(m+1,false);
         prev[0]=true;
-        for(int i=1;i<=n;i++)
-        {
+        for(int i=1;i<=n;i++){
             bool flag=true;
-            for(int t=1;t<=i;t++)
+            for(int x=1;x<=i;x++)
             {
-                if(p[t-1]!='*')
-                    flag=false;
+                if(p[x-1]!='*')   flag=false;
             }
             curr[0]=flag;
-            for(int j=1;j<=m;j++)
-            {
-                if(p[i-1]==s[j-1] || p[i-1]=='?') curr[j]=prev[j-1];
-                else if(p[i-1]=='*') curr[j]=prev[j] | curr[j-1];
+            for(int j=1;j<=m;j++){
+                if(s[j-1]==p[i-1] || p[i-1]=='?')  curr[j]=prev[j-1];
+                else if(p[i-1]=='*')   curr[j]=prev[j]|curr[j-1];
                 else curr[j]=false;
             }
             prev=curr;
