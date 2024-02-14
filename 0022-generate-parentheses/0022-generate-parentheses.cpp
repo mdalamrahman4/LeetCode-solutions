18df5e6f93e0ab5f1800dde12d69f772b25d3490
mdalamrahman4 <98524939+mdalamrahman4@users.noreply.github.com>
2024-02-14 17:28:26 +0530

    Time: 4 ms (50.30%), Space: 15.9 MB (24.72%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    void solve(string op, int open, int close, vector<string> &ans){
+    void f(string op, int open, int close, vector<string> &ans){
         if(open == 0 && close == 0){
             ans.push_back(op);
             return;
@@ -7,38 +7,33 @@ class Solution {
         //when count of open and close brackets are same then 
         //we have only one choice to put open bracket 
         if(open == close){
-            string op1 = op;
-            op1.push_back('(');
-            solve(op1, open-1, close, ans);
+            op.push_back('(');
+            f(op, open-1, close, ans);
         }
         else if(open == 0){
             //only choice is to put close brackets 
-            string op1 = op;
-            op1.push_back(')');
-            solve(op1, open, close-1, ans);
+            op.push_back(')');
+            f(op, open, close-1, ans);
         }
         else if(close == 0){
             //only choise is to use open bracket 
-            string op1 = op;
-            op1.push_back('(');
-            solve(op1, open-1, close, ans);
+            op.push_back('(');
+            f(op, open-1, close, ans);
         }
         else{
             string op1 = op;
             string op2 = op;
             op1.push_back('(');
             op2.push_back(')');
-            solve(op1, open-1, close, ans);
-            solve(op2, open, close-1, ans);
+            f(op1, open-1, close, ans);
+            f(op2, open, close-1, ans);
         }
     }
     public:
     vector<string> generateParenthesis(int n) {
-        int open = n;
-        int close = n;
         vector<string> ans;
         string op = "";
-        solve(op, open, close, ans);
+        f(op, n, n, ans);
         return ans;
     }
 };
