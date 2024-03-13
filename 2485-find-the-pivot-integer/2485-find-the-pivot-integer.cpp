6b498ef19b05d2b849f9fbb7185e4427317b5a6d
mdalamrahman4 <98524939+mdalamrahman4@users.noreply.github.com>
2024-03-13 08:22:40 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (73.82%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int pivotInteger(int n) {
         int lsum=0;
-        int total=0;
-        for(int i=1;i<=n;i++)   total+=i;
+        int total=(n*(n+1))/2;
         for(int i=1;i<=n;i++){
             lsum+=i;
             if(total-lsum+i==lsum)  return i;
