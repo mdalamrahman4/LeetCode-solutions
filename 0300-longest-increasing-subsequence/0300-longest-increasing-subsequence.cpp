b08d2ae903eb7939fe63d315cb2896ba961f49b1
mdalamrahman4 <98524939+mdalamrahman4@users.noreply.github.com>
2024-03-17 14:23:12 +0530

    Time: 3 ms (98.27%), Space: 12.8 MB (79.13%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,17 +4,15 @@ public:
         vector<int>temp;
         temp.push_back(nums[0]);
         int n=nums.size();
-        int l=1;
         for(int i=1;i<n;i++){
             if(nums[i]>temp.back()){
                 temp.push_back(nums[i]);
-                l++;
             }
             else{
                 int ind=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
                 temp[ind]=nums[i];
             }
         }
-        return l;
+        return temp.size();
     }
 };
