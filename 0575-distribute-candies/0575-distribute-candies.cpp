3d6d817ee7c4c25a019ff97dd797577a2dfe3c11
mdalamrahman4 <98524939+mdalamrahman4@users.noreply.github.com>
2024-03-17 14:30:08 +0530

    Time: 198 ms (60.70%), Space: 119.7 MB (52.46%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.cpp
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
-        unordered_map<int,int>mp;
-        for(auto &it:candyType){
-            mp[it]++;
-        }
-        int n=candyType.size();
-        int mx=n/2;
-        int all=mp.size();
-        return min(all,mx);
+        unordered_set<int>st(candyType.begin(),candyType.end());
+        return min(candyType.size()/2,st.size());
     }
 };
