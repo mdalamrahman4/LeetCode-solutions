bcfcff1cc6d40fc841e4d64870a3d06960f64f83
mdalamrahman4 <98524939+mdalamrahman4@users.noreply.github.com>
2024-03-21 19:49:32 +0530

    Time: 3 ms (97.46%), Space: 12.4 MB (70.68%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        vector<int>ans;
+        long long xorr=0;
         for(auto &it:nums){
-            mp[it]++;
+            xorr=xorr^it;
         }
-        for(auto &it:mp){
-            if(it.second==1)    ans.push_back(it.first);
+        int b1=0,b2=0;
+        long long rightmost=(xorr & (xorr-1))^xorr;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]&rightmost){
+                b1=b1^nums[i];
+            }
+            else{
+                b2=b2^nums[i];
+            }
         }
-        return ans;
+        return {b1,b2};
     }
 };
