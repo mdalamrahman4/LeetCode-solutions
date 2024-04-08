fc6d9cb60e4212ae8b347fda43143022fc17fad5
mdalamrahman4 <98524939+mdalamrahman4@users.noreply.github.com>
2024-04-08 22:22:31 +0530

    Time: 0 ms (100.00%), Space: 7 MB (75.31%) - LeetHub

--- 3099-harshad-number/3099-harshad-number.cpp
+++ 3099-harshad-number/3099-harshad-number.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int sumOfTheDigitsOfHarshadNumber(int x) {
-        string s=to_string(x);
         int sum=0;
-        for(int i=0;i<s.size();i++){
-            sum+=(s[i]-'0');
+        for(int n=x;n>0;n=n/10){
+            sum+=(n%10);
         }
         return (x%sum==0?sum:-1);
     }
