7ee828577c188924ea83b84436e400328c5d745f
mdalamrahman4 <98524939+mdalamrahman4@users.noreply.github.com>
2024-04-14 11:12:10 +0530

    Time: 107 ms (22.22%), Space: 110.2 MB (11.11%) - LeetHub

--- 3115-maximum-prime-difference/3115-maximum-prime-difference.cpp
+++ 3115-maximum-prime-difference/3115-maximum-prime-difference.cpp
@@ -1,26 +1,22 @@
-#include <vector>
-#include <unordered_set>
-using namespace std;
-
 class Solution {
 public:
     int maximumPrimeDifference(vector<int>& nums) {
-        vector<bool> isPrime(100, false);
-        unordered_set<int> primes = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
-        for (int p : primes) {
-            isPrime[p] = true;
-        }
-        
-        int n = nums.size();
-        int i = 0, j = n - 1;
-        while (i <= j) {
-            if (isPrime[nums[i]] && isPrime[nums[j]]) {
-                return j - i;
-            } else if (isPrime[nums[i]]) {
+        unordered_set<int>st={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
+        int n=nums.size();
+        int i=0,j=n-1;
+        while(i<=j){
+            auto it=st.find(nums[i]);
+            auto notit=st.find(nums[j]);
+            if(it!=st.end() && notit!=st.end()){
+                return j-i;
+            }
+            else if(it!=st.end()){
                 j--;
-            } else if (isPrime[nums[j]]) {
+            }
+            else if(notit!=st.end()){
                 i++;
-            } else {
+            }
+            else{
                 i++;
                 j--;
             }
