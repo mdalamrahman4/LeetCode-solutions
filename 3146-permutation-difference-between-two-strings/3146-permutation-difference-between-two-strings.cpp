68473157b853246750fd341b95ebeaa2fded96ad
mdalamrahman4 <98524939+mdalamrahman4@users.noreply.github.com>
2024-05-12 15:44:06 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (80.00%) - LeetHub

--- 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.cpp
+++ 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int findPermutationDifference(string s, string t) {
-        vector<int>arr1(26,0),arr2(26,0);
+        vector<pair<int,int>>arr(26);
         for(int i=0;i<s.size();i++){
-            arr1[s[i]-'a']=i;
-            arr2[t[i]-'a']=i;
+            arr[s[i]-'a'].first=i;
+            arr[t[i]-'a'].second=i;
         }
         int sum=0;
         for(int i=0;i<26;i++){
-            sum+=abs(arr1[i]-arr2[i]);
+            sum+=abs(arr[i].first-arr[i].second);
         }
         return sum;
     }
