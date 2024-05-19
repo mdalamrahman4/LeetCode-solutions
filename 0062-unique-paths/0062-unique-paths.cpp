5befac9c26d858485bd0b8703adbd5149f435f87
mdalamrahman4 <98524939+mdalamrahman4@users.noreply.github.com>
2024-05-19 17:38:17 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (17.09%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,23 +1,15 @@
 class Solution {
-    int countways(int m,int n){
-        vector<int>prev(n,0),curr(n,0);
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 || j==0 ){
-                    curr[j]=1;
-                    continue;
-                }
-                int up=0,left=0;
-                if(i>0) up=prev[j];
-                if(j>0) left=curr[j-1];
-                curr[j]=up+left;
-            }
-            prev=curr;
-        }
-        return prev[n-1];
-    }
 public:
+    int f(int i,int j,vector<vector<int>>&dp){
+        if(i==0 && j==0)    return 1;
+        if(i<0 || j<0)  return 0;
+        if(dp[i][j]!=-1)    return dp[i][j];
+        int up=f(i-1,j,dp);
+        int left=f(i,j-1,dp);
+        return dp[i][j]=up+left;
+    }
     int uniquePaths(int m, int n) {
-        return countways(m,n);
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return f(m-1,n-1,dp);
     }
 };
