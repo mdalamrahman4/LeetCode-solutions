933d84c1c54e1630abfc4f22b973244f9b82c059
mdalamrahman4 <98524939+mdalamrahman4@users.noreply.github.com>
2024-05-30 15:24:26 +0530

    Time: 12 ms (97.25%), Space: 31.1 MB (85.17%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return atmost(nums,goal)-atmost(nums,goal-1);
-    }
-    int atmost(vector<int>&nums,int goal){
-        if(goal<0)  return 0;
-        int n=nums.size();
-        int i=0,j=0;
+    int sliding(vector<int>&nums,int target){
+        if(target<0)    return 0;
+        int cnt=0;
+        int l=0,r=0,n=nums.size();
         int sum=0;
-        int ans=0;
-        while(j<n){
-            sum+=nums[j];
-            while(sum>goal){
-                sum-=nums[i++];
+        while(r<nums.size()){
+            sum+=nums[r];
+            while(sum>target){
+                sum-=nums[l];
+                l++;
             }
-            ans+=(j++-i+1);
+            cnt+=(r-l+1);
+            r++;
         }
-        return ans;
+        return cnt;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        /*int cnt=0;
+        for(int i=0;i<nums.size();i++){
+            int sum=0;
+            for(int j=i;j<nums.size();j++){
+                sum+=nums[j];
+                if(sum==goal)   cnt++;
+                else if(sum>goal)   break;
+            }
+        }
+        return cnt;
+        */
+        return sliding(nums,goal)-sliding(nums,goal-1);
     }
 };
