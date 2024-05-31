fabd46849e26ef092094f38c3fe598cc257bc345
mdalamrahman4 <98524939+mdalamrahman4@users.noreply.github.com>
2024-05-31 18:17:26 +0530

    Time: 129 ms (34.98%), Space: 55.8 MB (47.01%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
-    int fun(int k,vector<int> nums)
-    {
-        if(k==0) return 0;
-        unordered_map<int,int> mp;
-        int j=0;
-        int count=0;
-        int n=nums.size();
-        int total=0;
-        vector<int> freq(n+1,0);
-        for(int i=0;i<n;i++)
-        {
-            if(freq[nums[i]]==0) count++;
-            freq[nums[i]]++;
-            if(count<=k) total+=(i-j+1); // (i-j+1) = number of subarrays ending at i-th position which contains number of different integers less than or equal to k
-            else
-            {
-                while(count>k)
-                {
-                    freq[nums[j]]--;
-                    if(freq[nums[j]]==0)
-                    {
-                        count--;
-                    }
-                    j++;
-                }
-                total+=(i-j+1);
+    int sliding(vector<int>&nums,int k){
+        if(k<0) return 0;
+        int l=0,r=0,n=nums.size(),cnt=0;
+        unordered_map<int,int>mp;
+        while(r<n){
+            mp[nums[r]]++;
+            while(mp.size()>k){
+                mp[nums[l]]--;
+                if(mp[nums[l]]==0)  mp.erase(nums[l]);
+                l++;
             }
+            cnt+=(r-l+1);
+            r++;
         }
-        return total;
+        return cnt;
     }
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return fun(k,nums)-fun(k-1,nums);
+        return sliding(nums,k)-sliding(nums,k-1);
     }
 };
