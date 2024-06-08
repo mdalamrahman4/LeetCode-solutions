0b4231db392d28a3765528f5a6481bdcb99766f9
mdalamrahman4 <98524939+mdalamrahman4@users.noreply.github.com>
2024-06-08 17:59:07 +0530

    Time: 116 ms (99.08%), Space: 110 MB (99.93%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+        int sum=nums[0];
+        for(int r=1;r<n;r++){
+            if(nums[r]==0 && nums[r-1]==0){
+                return true;
+            }
+            sum+=nums[r];
+            if(sum%k==0)    return true;
+            int tempsum=sum,l=0;
+            while((r-l)>1 && tempsum>=k){
+                tempsum-=nums[l];
+                if(tempsum%k==0)    return true;
+                l++;
+            }
+        }
+        return false;
+    }
+};
