7722f78e261ec8461396263f7a09c968478770dc
mdalamrahman4 <98524939+mdalamrahman4@users.noreply.github.com>
2024-06-14 10:04:36 +0530

    Time: 120 ms (59.45%), Space: 69.2 MB (50.39%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=1;i<nums.size();i++){
             if(nums[i]<=nums[i-1]){
                 int diff=abs(nums[i]-nums[i-1]);
-                nums[i]+=(diff+1);
+                nums[i]=(nums[i-1]+1);
                 cnt+=(diff+1);
             }
         }
