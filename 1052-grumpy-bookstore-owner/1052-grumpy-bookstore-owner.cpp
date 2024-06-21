5978894c3bc1464ebe0694f220ccd41b0f38ed13
mdalamrahman4 <98524939+mdalamrahman4@users.noreply.github.com>
2024-06-21 08:11:19 +0530

    Time: 30 ms (25.59%), Space: 34.4 MB (16.52%) - LeetHub

---
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+        int satisfied=0,window=0,mxw=0;
+        int l=0;
+        for(int r=0;r<customers.size();r++){
+            if(grumpy[r]){
+                window+=customers[r];
+            }
+            else{
+                satisfied+=customers[r];
+            }
+            if(r-l+1>minutes){
+                if(grumpy[l]){
+                    window-=customers[l];
+                }
+                l++;
+            }
+            mxw=max(mxw,window);
+        }
+        return satisfied+mxw;
+    }
+};
