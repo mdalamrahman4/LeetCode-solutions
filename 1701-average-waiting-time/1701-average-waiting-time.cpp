cf617c575b05f951eebc068607430c154168a90a
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-07-09 11:00:50 +0530

    Time: 159 ms (35.88%), Space: 76.6 MB (57.65%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.cpp
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
         int st=customers[0][0];
-        long long cnt=0,n=customers.size();
+        double cnt=0;
+        int n=customers.size();
         for(int i=0;i<n;i++){
             st=max(st,customers[i][0]);
             int ft=st+customers[i][1];
             cnt+=(ft-customers[i][0]);
             st=ft;
         }
-        return (cnt/(n*1.0));
+        return cnt/n;
     }
 };
