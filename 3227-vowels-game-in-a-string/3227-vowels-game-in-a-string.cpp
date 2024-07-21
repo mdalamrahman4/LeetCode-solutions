bb5d4bb6e4a430a3406d0f0733dcdb3ed49781d8
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-07-21 19:12:18 +0530

    Time: 48 ms (60.00%), Space: 17.5 MB (20.00%) - LeetHub

---
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        for(auto &it:s){
+            if(it=='a' || it=='e' || it=='i' || it=='o' || it=='u') return true;
+        }
+        return false;
+    }
+};
