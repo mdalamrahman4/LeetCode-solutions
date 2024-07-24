aa60b14d5bbc270be4bc38453bf1c85aa0f54a2a
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-07-24 22:30:38 +0530

    Time: 3 ms (37.99%), Space: 8.4 MB (10.11%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,44 +1,31 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        int top=0,bottom=n-1,left=0,right=m-1;
-        int direction=0;
-        vector<int>dir;
-        while(left<=right && top<=bottom){
-            if(direction==0 && left<=right)
-            {
-                for(int i=left;i<=right;i++){
-                    dir.push_back(matrix[top][i]);
-                }
-                direction=1;
-                top++;
+        int top=0,bottom=matrix.size()-1,left=0,right=matrix[0].size()-1;
+        vector<int>spiral;
+        while(top<=bottom && left<=right)
+        {
+            for(int i=left;i<=right;i++){
+                spiral.push_back(matrix[top][i]);
             }
-            if(direction==1 && top<=bottom){
-                for(int i=top;i<=bottom;i++){
-                    dir.push_back(matrix[i][right]);
-                }
-                direction=2;
-                right--;
+            top++;
+            for(int i=top;i<=bottom;i++){
+                spiral.push_back(matrix[i][right]);
             }
-            if(direction==2 && right>=left){
-                for(int i=right;i>=left;i--){
-                    dir.push_back(matrix[bottom][i]);
-                }
-                bottom--;
-                direction=3;
+            right--;
+            if(top<=bottom){
+            for(int i=right;i>=left;i--){
+                spiral.push_back(matrix[bottom][i]);
             }
-            if(direction==3 && bottom>=top)
-            {
-                for(int i=bottom;i>=top;i--)
-                {
-                    dir.push_back(matrix[i][left]);
-                }
-                left++;
-                direction=0;
             }
+            bottom--;
+            if(left<=right){
+            for(int i=bottom;i>=top;i--){
+                spiral.push_back(matrix[i][left]);
+            }
+            }
+            left++;
         }
-        return dir;
+        return spiral;
     }
 };
