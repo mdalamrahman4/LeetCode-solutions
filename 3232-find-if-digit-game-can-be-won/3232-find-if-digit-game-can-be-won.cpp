bc71913a5489e54a48ce94f722dd7b9e3774f739
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-07-29 12:19:20 +0530

    Time: 4 ms (66.67%), Space: 27.9 MB (16.67%) - LeetHub

--- 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.cpp
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool canAliceWin(vector<int>& nums) {
         int sum=0;
-        for(auto&it:nums){
+        for(int &it:nums){
             sum+=(it<10)?it:-it;
         }
         return sum!=0;
