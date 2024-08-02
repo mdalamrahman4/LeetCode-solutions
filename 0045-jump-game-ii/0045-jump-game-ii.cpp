8d36da4910c0369b79d5bee0c47fe79b25d2e588
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-08-02 22:42:25 +0530

    Time: 12 ms (57.27%), Space: 19.3 MB (47.92%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
+        if(nums.size()==1)  return 0;
+        if(nums[0]==0)  return -1;
         int jumps=0,l=0,r=0,n=nums.size();
         while(r<n-1){
             int farthest=0;
