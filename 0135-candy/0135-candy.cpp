96d126a651eacb61ff622370a5565b3f08e75c12
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-08-03 14:55:37 +0530

    Time: 15 ms (55.23%), Space: 19.8 MB (77.23%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int sum=1,n=ratings.size(),i=1;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                sum++;
+                i++;
+                continue;
+            }
+            int peak=1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peak++;
+                sum+=peak;
+                i++;
+            }
+            int down=1;
+            while(i<n && ratings[i]<ratings[i-1]){
+                sum+=down;
+                down++;
+                i++;
+            }
+            if(down>peak)   sum+=(down-peak);
+        }
+        return sum;
+    }
+};
