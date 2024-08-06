6e280e03a556ff817f4860e57c351b50616c93a6
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-08-06 22:41:47 +0530

    Time: 310 ms (40.98%), Space: 181.5 MB (87.50%) - LeetHub

--- 3239-minimum-number-of-flips-to-make-binary-grid-palindromic-i/3239-minimum-number-of-flips-to-make-binary-grid-palindromic-i.cpp
+++ 3239-minimum-number-of-flips-to-make-binary-grid-palindromic-i/3239-minimum-number-of-flips-to-make-binary-grid-palindromic-i.cpp
@@ -6,18 +6,14 @@ public:
         for(int i=0;i<n;i++){
             int low=0,high=m-1;
             while(low<high){
-                if(grid[i][low]!=grid[i][high]) rcnt++;
-                low++;
-                high--;
+                if(grid[i][low++]!=grid[i][high--]) rcnt++;
             }
         }
         int ccnt=0;
         for(int i=0;i<m;i++){
             int low=0,high=n-1;
             while(low<high){
-                if(grid[low][i]!=grid[high][i]) ccnt++;
-                low++;
-                high--;
+                if(grid[low++][i]!=grid[high--][i]) ccnt++;
             }
         }
         return min(ccnt,rcnt);
