9a4e1682da2ced9df5cfec54115c7351eab20396
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-08-19 22:59:48 +0530

    Time: 6 ms (92.01%), Space: 9.4 MB (45.29%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int i=0,j=0;
+        for(auto it:moves){
+            if(it=='U') i--;
+            else if(it=='D')    i++;
+            else if(it=='L')    j--;
+            else j++;
+        }
+        return (i==0 && j==0);
+    }
+};
