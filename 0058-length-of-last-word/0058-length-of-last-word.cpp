9ffc97deacfc77c337fcfc6f81bb9c2fad5f3470
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-08-19 22:34:34 +0530

    Time: 4 ms (46.26%), Space: 8 MB (45.09%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int ans=0;
-        for(int i=s.length()-1;i>=0;i--)
-        {
-            if(s[i]!=' ')
-            {
-                ans++;
+        int len=0,n=s.size();
+        for(int i=n-1;i>=0;i--){
+            if(s[i]==' ' && len>0){
+                return len;
             }
-            else
-            {
-                if(ans>0) return ans;
+            else if(s[i]!=' '){
+                len++;
             }
         }
-        return ans;
+        return len;
     }
 };
