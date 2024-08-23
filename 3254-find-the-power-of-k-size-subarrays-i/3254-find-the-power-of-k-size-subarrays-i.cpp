3b92d02c0cdd5f4a27d4526ed4a6cc2fba037806
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-08-23 14:14:24 +0530

    Time: 23 ms (23.37%), Space: 33.2 MB (31.75%) - LeetHub

---
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool issortedc(vector<int>& nums, int start, int end) {
+        for (int i = start + 1; i <= end; ++i) {
+            if (nums[i] != nums[i - 1] + 1) return false;
+        }
+        return true;
+    }
+
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> ans;
+
+        for (int i = 0; i <= n - k; ++i) {
+            int j = i + k - 1;
+
+            if (issortedc(nums, i, j)) {
+                int mx = nums[i];  // Start with the first element of the current subarray
+                for (int m = i + 1; m <= j; ++m) {
+                    mx = max(mx, nums[m]);
+                }
+                ans.push_back(mx);
+            } else {
+                ans.push_back(-1);
+            }
+        }
+        
+        return ans;
+    }
+};
