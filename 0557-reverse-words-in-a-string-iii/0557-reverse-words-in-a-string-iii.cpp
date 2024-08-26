ded59672f5f77031240ececd21c01fcb61ff92f5
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-08-26 11:33:30 +0530

    Time: 10 ms (91.00%), Space: 11.1 MB (78.75%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            int j=i;
+            while(j<n && s[j]!=' '){
+                j++;
+            }
+            reverse(s.begin()+i,s.begin()+j);
+            i=j;
+        }
+        return s;
+    }
+};
