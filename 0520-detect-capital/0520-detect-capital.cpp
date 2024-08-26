92b72c6bd22d4716b4fbf7af6626d08d94e61962
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-08-26 13:36:51 +0530

    Time: 4 ms (30.07%), Space: 7.9 MB (14.85%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
-    bool iscapital(string word,int i,int j){
-        for(int x=i;x<=j;x++){
-            if(!isupper(word[x]))   return false;
-        }
-        return true;
-    }
-    bool notcapital(string word,int i,int j){
-        for(int x=i;x<=j;x++){
-            if(!islower(word[x]))   return false;
-        }
-        return true;
-    }
     bool detectCapitalUse(string word) {
-        int n=word.size();
-        return (iscapital(word,0,n-1) || notcapital(word,0,n-1) || notcapital(word,1,n-1));
+        int cntcapital=0,n=word.size();
+        for(char &it:word)  if(isupper(it)) cntcapital++;
+        return (cntcapital==0 || cntcapital==n || (isupper(word[0]) && cntcapital==1));
     }
 };
