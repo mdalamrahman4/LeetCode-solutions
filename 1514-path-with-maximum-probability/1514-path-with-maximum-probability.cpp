a13eb6c0ac040f6d51d60038f955f2f708f606a6
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-08-31 08:54:59 +0530

    Time: 155 ms (13.12%), Space: 73.9 MB (18.99%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        vector<vector<pair<int,double>>>adj(n);
-        for(int i=0;i<edges.size();i++)
-        {
-            adj[edges[i][0]].push_back({edges[i][1],succProb[i]});
-            adj[edges[i][1]].push_back({edges[i][0],succProb[i]});
+    typedef pair<double,int> p;
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+        unordered_map<int,vector<pair<int,double>>>adj;
+        vector<double>result(n,0);
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            double prob=succProb[i];
+            adj[u].push_back({v,prob});
+            adj[v].push_back({u,prob});
         }
-        vector<int>vis(n,0);
-        priority_queue<pair<double,int>>pq;
-        pq.push({double(1.0),start});
-        vector<double>mx(n,(double)0.0);
-        mx[start]=1.0;
-        while(!pq.empty())
-        {
-            auto it=pq.top();
+        priority_queue<p>pq;
+        result[start_node]=1;
+        pq.push({1.0,start_node});
+        while(!pq.empty()){
+            int currnode=pq.top().second;
+            double currprob=pq.top().first;
             pq.pop();
-            double proba=it.first;
-            int node=it.second;
-            if(!vis[node])
-            {
-                vis[node]=1;
-                for(auto &it:adj[node])
-                {
-                    double probb=it.second;
-                    int adjNode=it.first;
-                    if(proba*probb>mx[adjNode])
-                    {
-                        mx[adjNode]=proba*probb;
-                        pq.push({mx[adjNode],adjNode});
-                    }
+            for(auto &temp:adj[currnode]){
+                if(result[temp.first]<currprob*temp.second){
+                    result[temp.first]=currprob*temp.second;
+                    pq.push({result[temp.first],temp.first});
                 }
             }
         }
-        return mx[end];
+        return result[end_node];
     }
 };
