e160fb6dec75e595c26abeab172ef6377360d8df
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-09-15 13:28:09 +0530

    Time: 754 ms (5.12%), Space: 21.1 MB (6.74%) - LeetHub

--- 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -8,19 +8,19 @@ public:
         int res=0;
         for(int i=0;i<s.size();i++){
             if(s[i]=='a'){
-                state[0]=(state[0]+1)%2;
+                state[0]=(state[0]^1);
             }
             else if(s[i]=='e'){
-                state[1]=(state[1]+1)%2;
+                state[1]=(state[1]^1);
             }
             else if(s[i]=='i'){
-                state[2]=(state[2]+1)%2;
+                state[2]=(state[2]^1);
             }
             else if(s[i]=='o'){
-                state[3]=(state[3]+1)%2;
+                state[3]=(state[3]^1);
             }
             else if(s[i]=='u'){
-                state[4]=(state[4]+1)%2;
+                state[4]=(state[4]^1);
             }
             currstate="";
             for(int j=0;j<5;j++){
