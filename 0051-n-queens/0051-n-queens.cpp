0b68c7bb9728aa8c5e755a72adbbd5940924ae79
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-09-15 21:12:09 +0530

    Time: 6 ms (52.69%), Space: 9.6 MB (76.06%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
-    bool safe(int row,int col,vector<string>&board,int n)
-    {
+    bool issafe(int row,int col,vector<string>&board,int n){
         int duprow=row;
         int dupcol=col;
-        while(row>=0 && col>=0)
-        {
-            if(board[row][col]=='Q') return false;
+        while(row>=0 && col>=0){
+            if(board[row][col]=='Q')    return false;
             row--;
             col--;
         }
         row=duprow;
         col=dupcol;
-        while(col>=0)
-        {
-            if(board[row][col]=='Q') return false;
+        while(col>=0){
+            if(board[row][col]=='Q')    return false;
             col--;
         }
         col=dupcol;
-        while(row<n && col>=0)
-        {
-            if(board[row][col]=='Q') return false;
+        while(row<n && col>=0){
+            if(board[row][col]=='Q')    return false;
             row++;
             col--;
         }
         return true;
     }
-    void solve(int col,vector<string>&board,vector<vector<string>>&ans,int n)
-    {
+    void solve(int col,vector<string>&board,vector<vector<string>>&ans,int n){
         if(col==n){
             ans.push_back(board);
             return;
         }
-        for(int i=0;i<n;i++)
-        {
-            if(safe(i,col,board,n))
-            {
+        for(int i=0;i<n;i++){
+            if(issafe(i,col,board,n)){
                 board[i][col]='Q';
                 solve(col+1,board,ans,n);
                 board[i][col]='.';
@@ -46,8 +39,7 @@ public:
         vector<vector<string>>ans;
         vector<string>board(n);
         string s(n,'.');
-        for(int i=0;i<n;i++)
-        {
+        for(int i=0;i<n;i++){
             board[i]=s;
         }
         solve(0,board,ans,n);
