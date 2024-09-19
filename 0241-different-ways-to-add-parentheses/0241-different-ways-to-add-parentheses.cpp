626405b45867e1b102c1e97736674d48e83d8eed
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-09-19 14:32:12 +0530

    Time: 4 ms (46.04%), Space: 9.2 MB (88.77%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
+    unordered_map<string,vector<int>>mp;
     vector<int>f(string exp){
         vector<int>ans;
         int n=exp.size();
         for(int i=0;i<n;i++){
             if(exp[i]=='+' || exp[i]=='-' || exp[i]=='*'){
-                vector<int>left_res=f(exp.substr(0,i));
-                vector<int>right_res=f(exp.substr(i+1));
+                string str1=exp.substr(0,i),str2=exp.substr(i+1);
+                vector<int>left_res;
+                if(mp.find(str1)!=mp.end())
+                   left_res=mp[str1];
+                else    left_res=f(str1);
+                vector<int>right_res;
+                if(mp.find(str2)!=mp.end()){
+                    right_res=mp[str2];
+                }
+                else    right_res=f(str2);
                 for(auto &x:left_res){
                     for(auto &y:right_res){
                         if(exp[i]=='+'){
@@ -25,7 +34,8 @@ public:
         if(ans.empty()){
                 ans.push_back(stoi(exp));
             }
-            return ans;
+        mp[exp]=ans;
+        return ans;
     }
     vector<int> diffWaysToCompute(string exp) {
         return f(exp);
