cecd921ca8c9aa15c3135a3911eab5017d200a92
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-10-08 15:02:43 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (47.17%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int findComplement(int num) {
-        unsigned int mask=~0;
-        while(mask & num){
-            mask=mask<<1;
-        }
-        return ~mask^num;
+        int cnt=log2(num)+1;
+        int mask=(long)(1<<cnt)-1;
+        return mask^num;
     }
 };
