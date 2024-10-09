63a8597754a9834a10070b8065a6ceb184757347
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-10-09 10:32:10 +0530

    Time: 3 ms (42.94%), Space: 7.8 MB (81.46%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minAddToMakeValid(string s) {
         int cnt=0,scnt=0;
-        for(auto it:s){
+        for(char &it:s){
             if(it==')'){
                 if(scnt<=0)  cnt++;
                 else scnt--;
