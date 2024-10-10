4ea4e46b4a499f46de9a86a38dfcbeeaa2c5ae0b
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-10-10 17:03:29 +0530

    Time: 6 ms (5.35%), Space: 8.4 MB (44.48%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int no=abs(x);
+        int rev=0;
+        while(no){
+            int rem=no%10;
+            if(rev>(INT_MAX-rem)/10){
+                return 0;
+            }
+            rev=rev*10+rem;
+            no/=10;
+        }
+        return (x<no)?rev*-1:rev;
+    }
+};
