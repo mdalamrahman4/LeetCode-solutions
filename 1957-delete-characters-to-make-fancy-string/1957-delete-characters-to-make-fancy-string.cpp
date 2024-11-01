bcb647e319c4108a445157c33b9bba1a91e5a023
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-11-01 11:12:08 +0530

    Time: 26 ms (58.44%), Space: 42.8 MB (63.26%) - LeetHub

---
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        string ans="";
+        int cnt=1;
+        ans+=s[0];
+        for(int i=1;i<s.size();i++){
+            if(s[i]==s[i-1])    cnt++;
+            else cnt=1;
+            if(cnt<3){
+                ans+=s[i];
+            }
+        }
+        return ans;
+    }
+};
