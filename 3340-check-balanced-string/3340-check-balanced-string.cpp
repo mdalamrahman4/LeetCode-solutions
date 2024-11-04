516c888c710efa9044e1dca48034e8ed58e9e289
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-11-04 11:25:37 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (81.82%) - LeetHub

--- 3340-check-balanced-string/3340-check-balanced-string.cpp
+++ 3340-check-balanced-string/3340-check-balanced-string.cpp
@@ -3,7 +3,7 @@ public:
     bool isBalanced(string num) {
         int osum=0;
         bool oddy=true;
-        for(auto it:num){
+        for(auto &it:num){
             if(oddy)    osum+=(it-'0');
             else    osum-=(it-'0');
             oddy=!oddy;
