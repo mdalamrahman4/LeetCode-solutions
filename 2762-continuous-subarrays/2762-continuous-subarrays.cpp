9b3b337b85d948f0bd33a11aa8c803028c2bfd85
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-12-14 21:12:22 +0530

    Time: 176 ms (45.51%), Space: 129.7 MB (38.66%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.cpp
+++ 2762-continuous-subarrays/2762-continuous-subarrays.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
+    typedef pair<int,int> p;
     long long continuousSubarrays(vector<int>& nums) {
         int n=nums.size();
         int i=0,j=0;
         long long count=0;
-        map<int,int>mp;
+        priority_queue<p>mx;
+        priority_queue<p,vector<p>,greater<p>>mn;
         while(j<n){
-            mp[nums[j]]++;
-            while(abs(mp.rbegin()->first-mp.begin()->first)>2){
-                mp[nums[i]]--;
-                if(mp[nums[i]]==0)  mp.erase(nums[i]);
+            mx.push({nums[j],j});
+            mn.push({nums[j],j});
+            while(abs(mx.top().first-mn.top().first)>2){
                 i++;
+                while(!mx.empty() && mx.top().second<i){
+                    mx.pop();
+                }
+                while(!mn.empty() && mn.top().second<i){
+                    mn.pop();
+                }
             }
             count+=(j-i+1);
             j++;
