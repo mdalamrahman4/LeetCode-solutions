4f025f48c96136dc2979777e918b213d36d852ab
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2025-01-04 22:41:57 +0530

    Time: 0 ms, Memory: 27.3 MB - LeetHub

---
+++ 8825-1186-344-reverse-string/8825-1186-344-reverse-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void rev(vector<char>&s,int start,int end){
+        if(start>=end)  return;
+        swap(s[start],s[end]);
+        rev(s,start+1,end-1);
+    }
+    void reverseString(vector<char>& s) {
+        rev(s,0,s.size()-1);
+    }
+};
